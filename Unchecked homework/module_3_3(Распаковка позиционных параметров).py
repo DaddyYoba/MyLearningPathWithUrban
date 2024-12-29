23f05a31a3ff93770cd5e1a805b214c61f93f241
DaddyYoba <rohmanin156@mail.ru>
2024-12-29 20:12:20 +1000

    Самостоятельная работа по уроку "Распаковка позиционных параметров"

---
+++ Unchecked homework/module_3_3(Распаковка позиционных параметров).py
@@ -0,0 +1,16 @@
+def print_params(a = 1, b = 'строка', c = True):
+    global count
+    count += 1
+    print(f'Использование №{count}:\n'
+          f'a = {a}, тип: {type(a)}\n'
+          f'b = {b}, тип: {type(b)}\n'
+          f'c = {c}, тип: {type(c)}')
+
+count = 0
+values_list = ['НеСтрокаЧестноАвтичаю', ['Down', 'with', 'the,' 'sickness'], 69.420]
+values_dict = {'a': 19, 'b': ([([(["FFDP"])])],[([(["AILD"])], [(["Ice Nine Kills"])])]), 'c': False}
+values_list_2 = [54.32, 'Строка' ]
+
+print_params(*values_list)
+print_params(**values_dict)
+print_params(42, *values_list_2, )
